d3e569555eb0b360d15a662d20b9bf2fdaaad5a7
cole <57211534+crb596@users.noreply.github.com>
2021-09-23 00:02:44 +0400

    Update navigation.js

--- navigation.js
+++ navigation.js
@@ -33,24 +33,37 @@ function scrollUpdate(){
     let shift = 100; //Pixels to shift cutoff by
     //Home section
     if(document.body.scrollTop < window.innerHeight + shift){
+        document.getElementsByClassName("active")[0].classList.remove("active");
         let img = document.getElementById("homeImage");
-        img.source = "homeactive.png" //Set default
-
+        img.classList.add("active");
+        img.source = "homeactive.png" //Set active
     }
     //Film
     else if(document.body.scrollTop < window.innerHeight + window.innerHeight * 0.9 + shift){
+        document.getElementsByClassName("active")[0].classList.remove("active");
         document.getElementById("film").classList.add("active");
+        let img = document.getElementById("homeImage");
+        img.source = "home.png" //Set inactive
     }
     //Cast
     else if(document.body.scrollTop < window.innerHeight + window.innerHeight * 0.9 * 2 + shift){
+        document.getElementsByClassName("active")[0].classList.remove("active");
         document.getElementById("cast").classList.add("active");
+        let img = document.getElementById("homeImage");
+        img.source = "home.png" //Set inactive
     }
     //Reviews
     else if(document.body.scrollTop < window.innerHeight * 2 + window.innerHeight * 0.9 * 2 + shift){
+        document.getElementsByClassName("active")[0].classList.remove("active");
         document.getElementById("reviews").classList.add("active");
+        let img = document.getElementById("homeImage");
+        img.source = "home.png" //Set inactive
     }
     //Coming Soon
     else{
+        document.getElementsByClassName("active")[0].classList.remove("active");
         document.getElementById("comingSoon").classList.add("active");
+        let img = document.getElementById("homeImage");
+        img.source = "home.png" //Set inactive
     }
 }

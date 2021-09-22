31e1b0a570064ca0357e4486cd57fa57ad954af4
cole <57211534+crb596@users.noreply.github.com>
2021-09-22 23:31:13 +0400

    active

--- navigation.js
+++ navigation.js
@@ -1,18 +1,27 @@
 function move(location = "home"){
+    //Reassign active
+    document.getElementsByClassName("active").classList.remove("active");
+
     console.log(document.body.scrollTop);
     if(location == "home"){
         document.body.scrollTop = 0;
+        document.getElementById("homeImage").classList.add("active");
     }
     else if(location == "film"){
         document.body.scrollTop = parseInt(window.innerHeight);
+        document.getElementById("film").classList.add("active");
     }
     else if(location == "cast"){
         document.body.scrollTop = parseInt(window.innerHeight + window.innerHeight * 0.9);
+        document.getElementById("cast").classList.add("active");
     }
     else if(location == "reviews"){
         document.body.scrollTop = parseInt(window.innerHeight + window.innerHeight * 0.9 * 2);
+        document.getElementById("reviews").classList.add("active");
     }
     else if(location == "coming soon"){
         document.body.scrollTop = parseInt(window.innerHeight * 2 + window.innerHeight * 0.9 * 2);
+        document.getElementById("comingSoon").classList.add("active");
     }
+
 }

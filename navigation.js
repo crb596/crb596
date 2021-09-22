c62930d020c7fc3395d24ac0396b1fc646be3b62
cole <57211534+crb596@users.noreply.github.com>
2021-09-22 23:22:30 +0400

    Move

--- navigation.js
+++ navigation.js
@@ -1,18 +1,18 @@
-function homeButton(location = "home"){
+function move(location = "home"){
     console.log(document.body.scrollTop);
     if(location == "home"){
-
+        document.body.scrollTop = 0;
     }
     else if(location == "film"){
-
+        document.body.scrollTop = parseInt(window.innerHeight);
     }
     else if(location == "cast"){
-
+        document.body.scrollTop = parseInt(window.innerHeight + window.innerHeight * 0.9);
     }
     else if(location == "reviews"){
-
+        document.body.scrollTop = parseInt(window.innerHeight + window.innerHeight * 0.9 * 2);
     }
-    else if(location == "coming soon"){
-
+    else if(location == "comingsoon"){
+        document.body.scrollTop = parseInt(window.innerHeight * 2 + window.innerHeight * 0.9 * 2);
     }
 }

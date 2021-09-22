3490e07b6050f0d7d7f87823bcbc42059a0303e7
cole <57211534+crb596@users.noreply.github.com>
2021-09-23 00:09:07 +0400

    Update navigation.js

--- navigation.js
+++ navigation.js
@@ -1,9 +1,9 @@
-function move(location = "home"){
+function move(){
     //Reassign active
     document.getElementsByClassName("active")[0].classList.remove("active");
     
     let img = document.getElementById("homeImage");
-    img.source = "home.png" //Set default
+    img.src = "home.png" //Set default
 
     if(location == "home"){
         document.body.scrollTop = 0;
@@ -36,34 +36,34 @@ function scrollUpdate(){
         document.getElementsByClassName("active")[0].classList.remove("active");
         let img = document.getElementById("homeImage");
         img.classList.add("active");
-        img.source = "homeactive.png" //Set active
+        img.src = "homeactive.png" //Set active
     }
     //Film
     else if(document.body.scrollTop < window.innerHeight + window.innerHeight * 0.9 - shift){
         document.getElementsByClassName("active")[0].classList.remove("active");
         document.getElementById("film").classList.add("active");
         let img = document.getElementById("homeImage");
-        img.source = "home.png" //Set inactive
+        img.src = "home.png" //Set inactive
     }
     //Cast
     else if(document.body.scrollTop < window.innerHeight + window.innerHeight * 0.9 * 2 - shift){
         document.getElementsByClassName("active")[0].classList.remove("active");
         document.getElementById("cast").classList.add("active");
         let img = document.getElementById("homeImage");
-        img.source = "home.png" //Set inactive
+        img.src = "home.png" //Set inactive
     }
     //Reviews
     else if(document.body.scrollTop < window.innerHeight * 2 + window.innerHeight * 0.9 * 2 - shift){
         document.getElementsByClassName("active")[0].classList.remove("active");
         document.getElementById("reviews").classList.add("active");
         let img = document.getElementById("homeImage");
-        img.source = "home.png" //Set inactive
+        img.src = "home.png" //Set inactive
     }
     //Coming Soon
     else{
         document.getElementsByClassName("active")[0].classList.remove("active");
         document.getElementById("comingSoon").classList.add("active");
         let img = document.getElementById("homeImage");
-        img.source = "home.png" //Set inactive
+        img.src = "home.png" //Set inactive
     }
 }

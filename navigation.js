8321ff9a2f3691721e3a8583d9f514d67e1bbca1
cole <57211534+crb596@users.noreply.github.com>
2021-09-22 23:16:26 +0400

    Update navigation.js

---
+++ navigation.js
@@ -0,0 +1,18 @@
+function homeButton(location = "home"){
+    console.log(document.body.scrollTop);
+    if(location == "home"){
+
+    }
+    else if(location == "film"){
+
+    }
+    else if(location == "cast"){
+
+    }
+    else if(location == "reviews"){
+
+    }
+    else if(location == "coming soon"){
+
+    }
+}

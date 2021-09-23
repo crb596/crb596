6db2517ec8fa23f7c4ec7125ca4c085b5bbdd098
cole <57211534+crb596@users.noreply.github.com>
2021-09-23 14:05:25 +0400

    Documentation

--- navigation.js
+++ navigation.js
@@ -1,28 +1,33 @@
+//Called whenever the navigation bar is clicked, a string will be passed associated with each button
 function move(location){
     //Reassign active
     document.getElementsByClassName("active")[0].classList.remove("active");
     
     let img = document.getElementById("homeImage");
-    img.src = "home.png" //Set default
+    img.src = "home.png" //Set default home image
 
+    //Home button
     if(location == "home"){
         document.body.scrollTop = 0;
         img.classList.add("active");
-        img.src = "homeactive.png"
+        img.src = "homeactive.png";  //Light up home iamge with active one
     }
+    //Film button
     else if(location == "film"){
-        console.log("ran")
         document.body.scrollTop = parseInt(window.innerHeight);
         document.getElementById("film").classList.add("active");
     }
+    //Cast button
     else if(location == "cast"){
         document.body.scrollTop = parseInt((2 * window.innerHeight)-100);
         document.getElementById("cast").classList.add("active");
     }
+    //Review button
     else if(location == "reviews"){
         document.body.scrollTop = parseInt((3 * window.innerHeight)-2*100);
         document.getElementById("reviews").classList.add("active");
     }
+    //Coming soon button
     else if(location == "coming soon"){
         document.body.scrollTop = parseInt((4 * window.innerHeight)-3*100);
         document.getElementById("comingSoon").classList.add("active");
@@ -30,12 +35,10 @@ function move(location){
 
 }
 
+//Anytime onscroll event is called this function is called, reasigns active status depending on location of page
 function scrollUpdate(){
-    // console.log(document.body.scrollTop + " " + (window.innerHeight + window.innerHeight * 0.9))
     let shift = 100; //Pixels to shift cutoff by
     //Home section
-    console.log(document.body.scrollTop.toString() + ", " + ((3 * window.innerHeight)-2*100).toString())
-    console.log(document.body.scrollTop < window.innerHeight)
     if(document.body.scrollTop < window.innerHeight){
         document.getElementsByClassName("active")[0].classList.remove("active");
         let img = document.getElementById("homeImage");
